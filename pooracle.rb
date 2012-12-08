5bef4c740d1bf3f41f83d45ae3d9d614f7beeb2e
Ron <ron@skullsecurity.net>
2012-12-08 13:52:11 +0100

    Fixed a bug that came up when a block ended with 0x00 or 0x20 - I was using String.unpack('A#') (where # = blocksize) to split the blocks. It turns out, the 'A' operator in String.unpack() removes trailing NULLs and spaces, so I was getting shorter-than-expected blocks!

--- pooracle.rb
+++ pooracle.rb
@@ -29,10 +29,18 @@ class TestModule
     return @iv
   end
 
-  def initialize(test_string)
-    @key = (1..16).map{rand(255).chr}.join
-    @iv  = (1..16).map{rand(255).chr}.join
-    @encrypted = AES.encrypt(test_string, @key, @iv)
+  def initialize(data, key = nil, iv = nil, encrypted_data = nil)
+    @key = key.nil? ? (1..16).map{rand(255).chr}.join : key
+    @iv  = iv.nil?  ? (1..16).map{rand(255).chr}.join : iv
+    @encrypted = encrypted_data.nil? ? AES.encrypt(data, @key, @iv) : encrypted_data
+
+    puts()
+    puts("-" * 80)
+    puts("Generated test data:")
+    puts("key: #{@key.unpack("H*")}")
+    puts("iv:  #{@iv.unpack("H*")}")
+    puts("enc: #{@encrypted.unpack("H*")}")
+    puts("-" * 80)
   end
 
   def get_encrypted_string()
@@ -68,8 +76,6 @@ class PaddingOracle
       return ''
     end
 
-    #puts("Working on character #{character}...")
-
     # Try every value for the current character
     0.upto(255) do |i|
       # Set the current character to the new value
@@ -78,6 +84,9 @@ class PaddingOracle
       # Attempt to decrypt our fake block and the real block
       result = @module.attempt_decrypt(fakeblock, block)
       if(result)
+        puts("Character: #{character}")
+        puts("Fakeblock: #{fakeblock.unpack("H*")} (#{fakeblock.length} bytes)")
+        puts("Lastblock: #{lastblock.unpack("H*")} (#{lastblock.length} bytes)")
         result = fakeblock[character].ord ^ (block.length - character) ^ lastblock[character].ord
         #puts("Character %d might be %02x (%c)!" % [character, result, result])
 
@@ -96,20 +105,23 @@ class PaddingOracle
     return nil
   end
 
-  def go
+  def decrypt
     encrypted  = @module.get_iv() + @module.get_encrypted_string()
     blocksize  = @module.get_block_size()
     blockcount = encrypted.length / blocksize
 
     if(encrypted.length % @module.get_block_size() != 0)
-      puts("Encrypted data isn't a multiple of the length!")
+      puts("Encrypted data isn't a multiple of the blocksize! Is this a block cipher?")
     end
 
     puts("Encrypted length: %d" % encrypted.length)
     puts("Blocksize: %d" % blocksize)
     puts("Expected: %d blocks..." % blockcount)
 
-    blocks = encrypted.unpack("A#{blocksize}" * blockcount)
+    blocks = encrypted.unpack("a#{blocksize}" * blockcount)
+    blocks.each do |b|
+      puts(b.unpack("H*"))
+    end
 
     # Decrypt all the blocks - from the last to the first (after the IV)
     result = ''
@@ -131,11 +143,13 @@ end
 0.upto(64) do
   testdata = (0..rand(64)).map{rand(255).chr}.join
   mod = TestModule.new(testdata)
-  result = PaddingOracle.new(mod).go
+  result = PaddingOracle.new(mod).decrypt
   if(result != testdata)
     puts("ERROR: Data did not decrypt properly!")
     exit
   end
   puts(result.unpack("H*"))
 end
+
+puts(PaddingOracle.new(TestModule.new("ABCDEFGHIJKLMNOPQRSTUVWXYZ")).decrypt)
 
